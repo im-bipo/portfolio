519498f7124de809fa384c6a9fd5bd7b29661316
im-bipo <bipinkhatri.ram@gmail.com>
2024-06-19 14:41:51 +0545

    Basic Setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors:{
+        primary : '#191716',
+        action : '#EEE82C',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
