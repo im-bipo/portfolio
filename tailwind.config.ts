015f03a491bd084fbb020e73e750420bb1dee6bb
im-bipo <bipinkhatri.ram@gmail.com>
2024-06-24 19:04:21 +0545

    STYLED: Added twicks in tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,34 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1124px",
+      xl: "1280px",
+      "2xl": "1400px",
+    },
+
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      colors:{
-        primary : '#191716',
-        action : '#EEE82C',
+      colors: {
+        primary: "#191716",
+        action: "#EEE82C",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -20,5 +38,6 @@ const config: Config = {
     },
   },
   plugins: [],
-};
+} satisfies Config;
+
 export default config;
