a65088504ea222a63520c767c2c4206a003d6aaf
im-bipo <bipinkhatri.ram@gmail.com>
2025-04-06 18:53:09 +0545

    style: tech carousel set free to be scroll

--- src/components/custom/ProjectCard/TechnologyUsed.tsx
+++ src/components/custom/ProjectCard/TechnologyUsed.tsx
@@ -24,6 +24,7 @@ const TechnologyUsed = ({ technologies }: TechnologyUsedProps) => {
       }}
       opts={{
         loop: true,
+        dragFree: true,
       }}
       plugins={[
         AutoScroll({
